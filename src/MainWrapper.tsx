bc17c1b87085c866104cb4a55be47aee4e2cf7e7
f0lg0 <folgonileonardo@gmail.com>
2021-04-28 19:38:35 +0200

    new: basic system to check for screen width

--- src/MainWrapper.tsx
+++ src/MainWrapper.tsx
@@ -11,10 +11,35 @@ class MainWrapper extends React.Component<MainWrapperProps, MainWrapperState> {
     constructor(props: MainWrapperProps) {
         super(props);
         this.state = { isMobile: false };
+        this.checkScreenSize = this.checkScreenSize.bind(this);
+    }
+
+    checkScreenSize() {
+        const screenWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+
+        this.setState({ isMobile: screenWidth <= 760 ? true : false });
+    }
+    componentDidMount() {
+        window.addEventListener("resize", this.checkScreenSize);
+    }
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.checkScreenSize);
     }
 
     render() {
-        return <div className="wrapper"></div>;
+        return (
+            <div className="wrapper">
+                {this.state.isMobile ? (
+                    <div>
+                        <p>Mobile</p>
+                    </div>
+                ) : (
+                    <div>
+                        <p>Desktop</p>
+                    </div>
+                )}
+            </div>
+        );
     }
 }
 
