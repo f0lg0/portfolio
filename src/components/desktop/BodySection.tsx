8e86ed5c16912a1dbcb21e4a772c1dbe5d5bb751
f0lg0 <folgonileonardo@gmail.com>
2021-05-06 15:25:49 +0200

    new: started populating projects cards

--- src/components/desktop/BodySection.tsx
+++ src/components/desktop/BodySection.tsx
@@ -46,8 +46,19 @@ class BodySection extends React.Component<BodyProps, BodyState> {
                             language="C"
                             description="A simple emulator for the CHIP-8 interpreted programming language."
                             img_path="./assets/chip.png"
+                            expanded_content="A simple emulator for the CHIP-8 interpreted programming language written in C (SDL for graphics). The code is meant to be readable and clear, with a lot of comments. My main focus was the architecture, understanding opcodes and how things worked on a very low level."
+                            techDetails={[{ key: "Tech stack", val: "C, SDL library for the graphics." }]}
+                            github="https://github.com/f0lg0/CHIP-8"
                         />
-                        <ProjectCard title="Oncogene" language="Python" description="A Windows/Linux RAT written using TCP sockets." img_path="./assets/oncogene.png" />
+                        <ProjectCard
+                            title="Oncogene"
+                            language="Python"
+                            description="A Windows/Linux RAT written using TCP sockets."
+                            img_path="./assets/oncogene.png"
+                            expanded_content="A simple RAT written in Python 3 meant for educational/research purposes ONLY. Built for fun, Oncogene was my very first big project. The goal was to get into the cybersecurity world so I thought that making something by myself from scratch was the best way to get my hands dirty."
+                            techDetails={[{ key: "Tech stack", val: "Python 3 and related libraries" }]}
+                            github="https://github.com/f0lg0/Oncogene"
+                        />
                         <ProjectCard
                             title="StopCovid"
                             language="VueJS"
