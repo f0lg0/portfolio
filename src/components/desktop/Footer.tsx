36c9dd9af8c45781edb6572273cc2a1173428ad0
f0lg0 <folgonileonardo@gmail.com>
2021-06-11 18:02:46 +0200

    new: finished desktop footer

--- src/components/desktop/Footer.tsx
+++ src/components/desktop/Footer.tsx
@@ -16,7 +16,18 @@ class Footer extends React.Component<FooterProps, FooterState> {
                 <div className="left">
                     <p>© 2021 Leonardo Folgoni</p>
                 </div>
-                <div className="right"></div>
+                <div className="right">
+                    <div className="made_with">
+                        <div className="child">
+                            <a href="https://github.com/f0lg0/portfolio-dev" target="_blank" rel="noreferrer">
+                                Made with React
+                            </a>
+                        </div>
+                        <div className="react_wrapper">
+                            <img src="./assets/react.png" alt="React logo" />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
