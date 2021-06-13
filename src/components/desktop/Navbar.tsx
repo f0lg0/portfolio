cd2e92d75217c4ed7456678b88b32ae9b2d9acfa
f0lg0 <folgonileonardo@gmail.com>
2021-06-13 13:55:41 +0200

    edit: special links in navbar

--- src/components/desktop/Navbar.tsx
+++ src/components/desktop/Navbar.tsx
@@ -25,7 +25,7 @@ class Navbar extends React.Component<NavbarProps, NavbarState> {
                     <div className="link">
                         <a href="#Projects">Projects</a>
                     </div>
-                    <div className="link">
+                    <div className="link special_link">
                         <a href="#Blog">Blog</a>
                     </div>
                 </div>
@@ -65,16 +65,20 @@ class Navbar extends React.Component<NavbarProps, NavbarState> {
                         </div>
                     </div>
                     <div className="email">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5Z"
-                                stroke="#5A92E1"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                            />
-                            <path d="M3 7L12 13L21 7" stroke="#5A92E1" strokeLinecap="round" strokeLinejoin="round" />
-                        </svg>
+                        <div className="email_link">
+                            <a href="mailto:folgonileonardo@gmail.com">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5Z"
+                                        stroke="#5A92E1"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                    />
+                                    <path d="M3 7L12 13L21 7" stroke="#5A92E1" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
