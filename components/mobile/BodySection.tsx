adc6c0f09c0dc66454ee9eace74eedd512e00d96
f0lg0 <folgonileonardo@gmail.com>
2021-06-21 10:43:48 +0200

    new: work on mobile  project cards

--- components/mobile/BodySection.tsx
+++ components/mobile/BodySection.tsx
@@ -10,24 +10,28 @@ export default function BodySection() {
                     language="Typescript"
                     description="A platform where students can share their school work."
                     github="https://api.github.com/repos/capihub/capihub"
+                    backup_url="https://github.com/capihub/capihub"
                 />
                 <ProjectCard
                     title="CHIP-8 Emulator"
                     language="C"
                     description="A simple emulator for the CHIP-8 interpreted programming language."
                     github="https://api.github.com/repos/f0lg0/CHIP-8"
+                    backup_url="https://github.com/f0lg0/CHIP-8"
                 />
                 <ProjectCard
                     title="Oncogene"
                     language="Python"
                     description="A Windows/Linux RAT written using TCP sockets."
                     github="https://api.github.com/repos/f0lg0/Oncogene"
+                    backup_url="https://github.com/f0lg0/Oncogene"
                 />
                 <ProjectCard
                     title="StopCovid"
                     language="VueJS"
                     description="A website to track and analyze Covid-19 in Italy"
                     github="https://api.github.com/repos/f0lg0/stopcovid-website"
+                    backup_url="https://github.com/f0lg0/stopcovid-website"
                 />
             </div>
         </div>
--- components/mobile/ProjectCard.tsx
+++ components/mobile/ProjectCard.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import styles from "../../styles/Project.Card.Mobile.module.css";
 
 interface ProjectCardProps {
-    title: String;
-    description: String;
-    language: String;
-    github: String;
+    title: string;
+    description: string;
+    language: string;
+    github: string | undefined;
+    backup_url: string | null;
 }
 interface ProjectCardState {
     stars: string | undefined;
+    url: string;
 }
 
 class ProjectCard extends React.Component<ProjectCardProps, ProjectCardState> {
@@ -16,6 +18,7 @@ class ProjectCard extends React.Component<ProjectCardProps, ProjectCardState> {
         super(props);
         this.state = {
             stars: undefined,
+            url: this.props.backup_url ? this.props.backup_url : "https://github.com/f0lg0",
         };
     }
 
@@ -27,21 +30,45 @@ class ProjectCard extends React.Component<ProjectCardProps, ProjectCardState> {
             if (repo.message) {
                 this.setState({ stars: "Not found" });
             } else {
-                this.setState({ stars: repo.stargazers_count.toString() });
+                this.setState({ stars: repo.stargazers_count.toString(), url: repo.html_url });
             }
         } catch (err) {
             console.error(err);
         }
+
+        // this.setState({ stars: "34" });
     }
 
     render() {
         return (
-            <div className={styles.projectcard_mobile}>
-                <p className={styles.language_mobile}>{this.props.language}</p>
-                <h1 className={styles.title_mobile}>{this.props.title}</h1>
-                <p className={styles.description_mobile}>{this.props.description}</p>
-                <div className={styles.github_mobile}>{this.state.stars}</div>
-            </div>
+            <a href={this.state.url} target="_blank" className={styles.wrapper}>
+                <div className={styles.projectcard_mobile}>
+                    <div className={styles.content}>
+                        <p className={styles.language}>{this.props.language.toUpperCase()}</p>
+                        <h1 className={styles.title}>{this.props.title}</h1>
+                        <p className={styles.description}>{this.props.description}</p>
+                        <div className={styles.github_stars}>
+                            <div className={styles.icon}>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="1"
+                                    stroke="#ffffff"
+                                    fill="#ffffff"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+                                </svg>
+                            </div>
+                            <p className={styles.amt}>{this.state.stars}</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
         );
     }
 }
