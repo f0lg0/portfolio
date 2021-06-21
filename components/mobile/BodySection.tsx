ef05573fad16af7a099a39de70d6266b53fd1f9f
f0lg0 <folgonileonardo@gmail.com>
2021-06-21 10:09:13 +0200

    new: getting number of stars from github

--- components/mobile/BodySection.tsx
+++ components/mobile/BodySection.tsx
@@ -9,25 +9,25 @@ export default function BodySection() {
                     title="CapiHub"
                     language="Typescript"
                     description="A platform where students can share their school work."
-                    github="https://github.com/capihub/"
+                    github="https://api.github.com/repos/capihub/capihub"
                 />
                 <ProjectCard
                     title="CHIP-8 Emulator"
                     language="C"
                     description="A simple emulator for the CHIP-8 interpreted programming language."
-                    github="https://github.com/f0lg0/CHIP-8"
+                    github="https://api.github.com/repos/f0lg0/CHIP-8"
                 />
                 <ProjectCard
                     title="Oncogene"
                     language="Python"
                     description="A Windows/Linux RAT written using TCP sockets."
-                    github="https://github.com/f0lg0/Oncogene"
+                    github="https://api.github.com/repos/f0lg0/Oncogene"
                 />
                 <ProjectCard
                     title="StopCovid"
                     language="VueJS"
                     description="A website to track and analyze Covid-19 in Italy"
-                    github="https://github.com/f0lg0/stopcovid-website"
+                    github="https://api.github.com/repos/f0lg0/stopcovid-website"
                 />
             </div>
         </div>
--- components/mobile/ProjectCard.tsx
+++ components/mobile/ProjectCard.tsx
@@ -7,21 +7,40 @@ interface ProjectCardProps {
     language: String;
     github: String;
 }
-interface ProjectCardState {}
+interface ProjectCardState {
+    stars: string | undefined;
+}
 
 class ProjectCard extends React.Component<ProjectCardProps, ProjectCardState> {
     constructor(props: ProjectCardProps) {
         super(props);
-        this.state = {};
+        this.state = {
+            stars: undefined,
+        };
+    }
+
+    async componentDidMount() {
+        try {
+            const res = await fetch(`${this.props.github}`);
+            const repo = await res.json();
+
+            if (repo.message) {
+                this.setState({ stars: "Not found" });
+            } else {
+                this.setState({ stars: repo.stargazers_count.toString() });
+            }
+        } catch (err) {
+            console.error(err);
+        }
     }
 
     render() {
         return (
             <div className={styles.projectcard_mobile}>
-                <p className={styles.language_mobile}></p>
-                <h1 className={styles.title_mobile}></h1>
-                <p className={styles.description_mobile}></p>
-                <div className={styles.github_mobile}></div>
+                <p className={styles.language_mobile}>{this.props.language}</p>
+                <h1 className={styles.title_mobile}>{this.props.title}</h1>
+                <p className={styles.description_mobile}>{this.props.description}</p>
+                <div className={styles.github_mobile}>{this.state.stars}</div>
             </div>
         );
     }
