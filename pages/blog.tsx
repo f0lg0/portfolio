071c1e089a801d6b26be211ce056d85ead053de2
f0lg0 <folgonileonardo@gmail.com>
2021-06-22 16:52:27 +0200

    new: started to look at The Blog

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,3 +1,13 @@
+import styles from "../styles/Blog.module.css";
+import BlogLogo from "../public/theblog.svg";
+import Image from "next/image";
+
 export default function Blog() {
-    return <p>Blog!</p>;
+    return (
+        <div className={styles.main}>
+            <div className={styles.blog_title}>
+                <Image src={BlogLogo} alt="THE BLOG" />
+            </div>
+        </div>
+    );
 }
