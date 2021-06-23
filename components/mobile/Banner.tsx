7ba0836de228eb95a35a9a004edc14767c6c0f0b
f0lg0 <folgonileonardo@gmail.com>
2021-06-23 10:28:40 +0200

    edit: switched to next/image banner mobile

--- components/mobile/Banner.tsx
+++ components/mobile/Banner.tsx
@@ -1,10 +1,13 @@
 import styles from "../../styles/Banner.Mobile.module.css";
+import Image from "next/image";
 
 export default function Banner() {
     return (
         <div className={styles.banner_mobile}>
             <div className={styles.big_logo + " " + styles.fade_in_image}>
-                <img src="/logo.png" alt="LF" />
+                <div className={styles.big_logo_img}>
+                    <Image src="/logo.png" alt="LF" layout="fill" objectFit="cover" />
+                </div>
             </div>
             <div className={styles.intro}>
                 <div className={styles.top_bar}>
