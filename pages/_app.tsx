ed9c5f0511bdb38d8b739869c50f3f9e51cba295
f0lg0 <folgonileonardo@gmail.com>
2021-06-23 11:01:27 +0200

    new: favicon and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <main>
+            <Head>
+                <title>f0lg0</title>
+            </Head>
+            <Component {...pageProps} />;
+        </main>
+    );
 }
 export default MyApp;
