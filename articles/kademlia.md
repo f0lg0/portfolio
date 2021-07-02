d1dc0e1586a048d7e731e57baff7c6a3caa6174c
f0lg0 <folgonileonardo@gmail.com>
2021-07-02 15:55:34 +0200

    new: view article page

--- articles/kademlia.md
+++ articles/kademlia.md
@@ -1,6 +1,6 @@
 ---
 title: "Graphical state dumping in a Kademlia network"
-date: "July 1 2021"
+date: "2021-07-01"
 excerpt: "Overview of my Kademlia DHT Rust implementation, with graphical state dumping features for a better debugging experience. In this article I will analyze the dumping techniques used in my library"
 cover_image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F2%2F20%2FChord_network.png&f=1&nofb=1"
 ---
---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,74 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import marked from "marked";
+
+import styles from "../../styles/ArticlePage.Desktop.module.css";
+
+interface Paths {
+    params: {
+        slug: string;
+    };
+}
+
+interface ArticleFrontmatter {
+    title: string;
+    date: string;
+    excerpt: string;
+    cover_image: string;
+}
+
+interface ArticlePageProps {
+    frontmatter: ArticleFrontmatter;
+    slug: string;
+    content: string;
+}
+
+export default function ArticlePage(article: ArticlePageProps) {
+    return (
+        <div className={styles.article_body}>
+            <div className={styles.metadata}>
+                <p>{article.frontmatter.date}</p>
+                <h1>{article.frontmatter.title}</h1>
+                <div className={styles.excerpt}>
+                    <div className={styles.line}></div>
+                    <p>{article.frontmatter.excerpt}</p>
+                    <div className={styles.line}></div>
+                </div>
+            </div>
+
+            <div className="content">
+                <div dangerouslySetInnerHTML={{ __html: marked(article.content) }}></div>
+            </div>
+        </div>
+    );
+}
+
+export async function getStaticPaths() {
+    const files = fs.readdirSync(path.join("articles"));
+
+    const paths = files.map((filename) => ({
+        params: {
+            slug: filename.replace(".md", ""),
+        },
+    }));
+
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({ params: { slug } }: Paths) {
+    const markdownWithMeta = fs.readFileSync(path.join("articles", slug + ".md"), "utf-8");
+
+    const { data: frontmatter, content } = matter(markdownWithMeta);
+
+    return {
+        props: {
+            frontmatter,
+            slug,
+            content,
+        },
+    };
+}
