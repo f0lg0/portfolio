e73c9200327019264fb6a7273657bc325a2033e9
f0lg0 <folgonileonardo@gmail.com>
2021-07-02 14:54:57 +0200

    fix: better articles layout

--- components/desktop/BlogFeed.tsx
+++ components/desktop/BlogFeed.tsx
@@ -1,5 +1,6 @@
 import styles from "../../styles/Blog.Desktop.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 interface ArticleFrontmatter {
     title: string;
@@ -22,25 +23,22 @@ export default function BlogFeed({ posts }: BlogProps) {
         <div className={styles.wrapper}>
             <div className={styles.featured}>
                 <h2>Featured</h2>
-                <div
-                    className={styles.article_wrapper}
-                    onClick={() => {
-                        alert("Coming soon!");
-                    }}
-                >
-                    <div className={styles.cover_image}>
-                        <Image src={posts[0].frontmatter.cover_image} alt="Thumbnail" objectFit="fill" layout="fill" />
-                    </div>
-                    <div className={styles.main_article_details}>
-                        <h3>{posts[0].frontmatter.title}</h3>
-                        <div className={styles.desc}>
-                            <div className={styles.fade}></div>
-                            <pre className={styles.preserve_text}>
-                                <p>{posts[0].frontmatter.excerpt}</p>
-                            </pre>
+                <Link href={`/blog/${posts[0].slug}`} passHref>
+                    <div className={styles.article_wrapper}>
+                        <div className={styles.cover_image}>
+                            <Image src={posts[0].frontmatter.cover_image} alt="Thumbnail" objectFit="fill" layout="fill" />
+                        </div>
+                        <div className={styles.main_article_details}>
+                            <h3>{posts[0].frontmatter.title}</h3>
+                            <div className={styles.desc}>
+                                <div className={styles.fade}></div>
+                                <pre className={styles.preserve_text}>
+                                    <p>{posts[0].frontmatter.excerpt}</p>
+                                </pre>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </Link>
             </div>
 
             <div className={styles.sep}>
@@ -51,22 +49,25 @@ export default function BlogFeed({ posts }: BlogProps) {
                 <div className={styles.articles}>
                     {posts.map((post, index) => {
                         return (
-                            <div
-                                className={styles.article}
-                                key={index}
-                                onClick={() => {
-                                    alert("Coming soon!");
-                                }}
-                            >
-                                <div className={styles.thumbnail}>
-                                    <Image src={post.frontmatter.cover_image} layout="fill" objectFit="fill" />
-                                </div>
-                                <div className={styles.text}>
-                                    <p className={styles.article_date}>{post.frontmatter.date}</p>
-                                    <h2>{post.frontmatter.title}</h2>
+                            <div className={styles.grid_element}>
+                                <Link href={`/blog/${post.slug}`} key={index}>
+                                    <div className={styles.article}>
+                                        <div className={styles.thumbnail}>
+                                            <Image src={post.frontmatter.cover_image} layout="fill" objectFit="fill" />
+                                        </div>
+                                        <div className={styles.text}>
+                                            <p className={styles.article_date}>{post.frontmatter.date}</p>
+                                            <h2>{post.frontmatter.title}</h2>
 
-                                    <p className={styles.article_desc}>{post.frontmatter.excerpt}</p>
-                                </div>
+                                            <div className={styles.desc_wrapper}>
+                                                <div className={styles.small_fade}></div>
+                                                <pre className={styles.preserve_text}>
+                                                    <p className={styles.article_desc}>{post.frontmatter.excerpt}</p>
+                                                </pre>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Link>
                             </div>
                         );
                     })}
